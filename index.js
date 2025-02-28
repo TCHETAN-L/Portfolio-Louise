f733f9fb6eaba02f3b996fee70fe173653724fc4
Louise <louisetchetan@gmail.com>
2025-02-28 13:03:59 +0100

    Portfolio-Louise

---
+++ index.js
@@ -0,0 +1,9 @@
+// Permet de boucler l'animation indéfiniment
+
+var typed = new Typed(".text", {
+    strings: ["géomaticienne", "développeuse web", "cartographe"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true 
+});
